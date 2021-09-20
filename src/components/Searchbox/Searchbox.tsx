3b246a3924750d840167b4a95a5ad65ff5286c92
Sunil <sunilnet4@gmail.com>
2021-09-20 23:09:30 +0530

    wip

---
+++ src/components/Searchbox/Searchbox.tsx
@@ -0,0 +1,10 @@
+import './Searchbox.css';
+import React from 'react';
+
+const Searchbox = () => {
+    return(
+        <input type="search" className="search" placeholder="Search Pokemons" />
+    )
+}
+
+export default Searchbox;
