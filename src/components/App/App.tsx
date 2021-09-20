6983b57129cffc004ba983a52209f82d376c993b
Sunil <sunilnet4@gmail.com>
2021-09-20 23:08:33 +0530

    wip

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { pokemonData } from '../../data/pokemonData';
 import Pokedex from '../Pokedex/Pokedex';
 import './App.css';
 
-class App extends React.Component {
+interface AppState {
+    searchField: string,
+    allPokemons: any,
+    searchedPokemons: any,
+    selectedPokemon: any
+}
+
+class App extends React.Component<any, AppState> {
+    state = {
+        searchField: "",
+        allPokemons: [],
+        searchedPokemons: [],
+        selectedPokemon: undefined
+    }
+
+    componentDidMount () {
+        const pokeData = pokemonData;
+    }
+
     render() {
         return(
             <div className="App">
