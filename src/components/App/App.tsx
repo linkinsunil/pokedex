b1567636995241d6e57132c5458e99a018628805
Sunil <sunilnet4@gmail.com>
2021-09-20 23:48:57 +0530

    update: updated componentdidmount

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { pokemonData } from '../../data/pokemonData';
+import { PokemonSchema, PokemonSpritesSchema, UnpatchedPokemonSchema } from '../../types/PokemonSchema';
 import Pokedex from '../Pokedex/Pokedex';
 import './App.css';
 
 interface AppState {
-    searchField: string,
-    allPokemons: any,
-    searchedPokemons: any,
-    selectedPokemon: any
+    searchField: string;
+    allPokemons: PokemonSchema[];
+    searchedPokemons: PokemonSchema[];
+    selectedPokemon: PokemonSchema | undefined;
 }
 
 class App extends React.Component<any, AppState> {
@@ -18,8 +19,39 @@ class App extends React.Component<any, AppState> {
         selectedPokemon: undefined
     }
 
-    componentDidMount () {
-        const pokeData = pokemonData;
+    patchPokemonData = (pokemons: UnpatchedPokemonSchema[]) => {
+        const patchedPokemons= pokemons.map((pokemon) => {
+            let parsedSprites: PokemonSpritesSchema = {
+                normal: undefined,
+                animated: undefined
+            }
+
+            try{
+                parsedSprites = pokemon.sprites && JSON.parse(pokemon.sprites);
+            }catch(e){
+                console.log("Exception while parsing the sprites: ", e);
+            }
+
+            const patchedPokemon: PokemonSchema = {
+                ...pokemon,
+                sprites: parsedSprites
+            }
+
+            return patchedPokemon;
+        })
+
+        return patchedPokemons;
+    }
+
+    componentDidMount() {
+        //Patch the stringified pokemon sprites
+        const patchedPokemons: PokemonSchema[] = this.patchPokemonData(pokemonData)
+
+        //Update the state with patched pokemons
+        this.setState({
+            allPokemons: patchedPokemons,
+            searchedPokemons: patchedPokemons
+        })
     }
 
     render() {
